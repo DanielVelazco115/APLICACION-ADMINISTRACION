792625d4ea4a983ad4b2ae0f23704ffaabb770ec
DanielVelazco115 <j.filemon@ranchosantarosa.com.mx>
2026-03-29 10:12:23 -0600

    nterfaz_Nomina_Azu.py

--- Paginas/NOMINA/Interfaz_Nomina_Azu.py
+++ Paginas/NOMINA/Interfaz_Nomina_Azu.py
@@ -3,4 +3,5 @@ import streamlit as st
 def mostrar_interfaz_nomina_azu():
     st.title("Gestión de Contador")
     st.subheader("Reportes de periodos bimestrales")
+     st.subheader("Aun no disponible")
     # ... Pega aquí todo el contenido de la sección de contador ...
