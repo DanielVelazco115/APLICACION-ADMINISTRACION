0bb7cd95b0181f873dbefca0996b4ca17daa9a22
DanielVelazco115 <j.filemon@ranchosantarosa.com.mx>
2026-04-03 09:55:19 -0600

    Update Interfaz_Principal.py

--- Interfaz_Principal.py
+++ Interfaz_Principal.py
@@ -75,7 +75,7 @@ def leer_ultima_fecha():
     return "Sin registros previos"
 
 # --- 3. ESTILOS CSS - CENTRADO TOTAL ---
-fondo_base64 = get_base64_image("Imagenes/Log.jpg") 
+fondo_base64 = get_base64_image("Imagenes/Log2.jpg") 
 
 if fondo_base64:
     st.markdown(f"""
